4bf4c7201c4355fc5a6ca1228657acbf347d13e3
Lily She-Yin (MIPT) <lily.she-yin@hmrc.gov.uk>
2024-03-19 13:13:57 +0000

    Update app service plan tier from Free to Standard

--- InfrastructureAsCode/mainTerra.tf
+++ InfrastructureAsCode/mainTerra.tf
@@ -34,7 +34,7 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   resource_group_name = azurerm_resource_group.rg.name
 
   sku {
-    tier = "Free"
+    tier = "Standard"
     size = local.sku
   }
 }
